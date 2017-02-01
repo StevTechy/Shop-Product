df60d58e8c145d472acdd13741498c4d6dd7091f
Steven Hooper <stevenms@outlook.com>
2017-02-01 21:09:00 +0000

    Adding search functionality and paging

--- Shop Product.Viewer/Controllers/HomeController.cs
+++ Shop Product.Viewer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Shop_Product.Data;
+﻿using PagedList;
+using Shop_Product.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +11,13 @@ namespace Shop_Product.Viewer.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Index(int? page, string name = null)
         {
-            return View(new ShopConnection().GetProducts());
+            var products = new ShopConnection().GetProducts().AsQueryable();
+
+            if (!string.IsNullOrEmpty(name)) products = products.Where(a => a.Name != null && a.Name.ToLower().Contains(name.ToLower()));
+
+            return View(products.OrderBy(x => x.Name).ToList().ToPagedList(page ?? 1, 20));
         }
     }
 }
